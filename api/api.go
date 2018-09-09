86f821e397491fbcbc09de5f3fd46cf26b95df25
bitebit <kfbygj@gmail.com>
2018-09-09 21:19:48 +0800

    feat(API): 封装小程序API调用功能，支持token

--- api/api.go
+++ api/api.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"fmt"
 	"net/url"
+	"time"
 
 	"github.com/parnurzeal/gorequest"
 )
@@ -11,18 +12,33 @@ import (
 type (
 	// WechatAPI 小程序API
 	WechatAPI struct {
-		appID     string
-		appKey    string
-		apiScheme string
-		apiDomain string
-		apiToken  *WechatToken
+		appID            string
+		appKey           string
+		apiScheme        string
+		apiDomain        string
+		apiToken         string
+		apiTokenExpireIn int
+		apiTokenExpireAt time.Time
+		apiTokenStore    WechatTokenStore
+	}
+
+	// WechatTokenStore 微信token存储器
+	WechatTokenStore interface {
+		Get() (token string, exipresIn int, expireAt time.Time)
+		Set(token string, exipresIn int, expireAt time.Time)
 	}
 
 	// WechatResp 微信接口响应
 	WechatResp struct {
 		ErrCode int    `json:"errcode"`
 		ErrMsg  string `json:"errmsg"`
-		Data    interface{}
+		Data    []byte
+	}
+
+	// WechatRespToken 微信token响应结果
+	WechatRespToken struct {
+		Token     string `json:"access_token"`
+		ExpiresIn int    `json:"expires_in"`
 	}
 
 	option struct {
@@ -35,13 +51,13 @@ type (
 )
 
 // NewWechatAPI 生成一个api
-func NewWechatAPI(appID, appKey string, apiToken *WechatToken) *WechatAPI {
+func NewWechatAPI(appID, appKey string, tokenStore WechatTokenStore) *WechatAPI {
 	return &WechatAPI{
-		apiScheme: "https",
-		apiDomain: "api.weixin.qq.com",
-		appID:     appID,
-		appKey:    appKey,
-		apiToken:  apiToken,
+		apiScheme:     "https",
+		apiDomain:     "api.weixin.qq.com",
+		appID:         appID,
+		appKey:        appKey,
+		apiTokenStore: tokenStore,
 	}
 }
 
@@ -64,7 +80,7 @@ func (api *WechatAPI) Request(opt *option) (*WechatResp, []error) {
 	req.Query(opt.query).Send(opt.body)
 
 	if opt.withToken {
-		req.Query(fmt.Sprintf("access_token=%s", api.apiToken.GetToken()))
+		req.Query(fmt.Sprintf("access_token=%s", api.GetToken()))
 	}
 
 	_, body, errs := req.End()
@@ -84,6 +100,73 @@ func (api *WechatAPI) Request(opt *option) (*WechatResp, []error) {
 	}
 
 	// 业务成功
-	wechatResp.Data = body
+	wechatResp.Data = []byte(body)
 	return wechatResp, nil
+}
+
+// GetToken 获取token
+// 优先 内存获取
+// 再次 store获取
+// 再次 生成并保存
+func (api *WechatAPI) GetToken() string {
+	if api.IsValid() {
+		return api.apiToken
+	}
+
+	token, exipresIn, expireAt := api.apiTokenStore.Get()
+	if isValid(token, expireAt) {
+		api.apiToken = token
+		api.apiTokenExpireIn = exipresIn
+		api.apiTokenExpireAt = expireAt
+
+		return token
+	}
+
+	api.RenewToken()
+
+	return api.apiToken
+}
+
+// RenewToken 重新生成一个token
+func (api *WechatAPI) RenewToken() (*WechatResp, []error) {
+	query := map[string]string{}
+	query["grant_type"] = "client_credential"
+	query["appid"] = api.appID
+	query["secret"] = api.appKey
+
+	resp, errs := api.Request(&option{
+		method:    "GET",
+		url:       "/cgi-bin/token",
+		withToken: false,
+		query:     query,
+	})
+
+	// 请求成功，解析内容
+	if resp.ErrCode == 0 {
+		respToken := &WechatRespToken{}
+
+		err := json.Unmarshal(resp.Data, respToken)
+		if err != nil {
+			return resp, []error{err}
+		}
+
+		api.apiToken = respToken.Token
+		api.apiTokenExpireIn = respToken.ExpiresIn
+		api.apiTokenExpireAt = time.Now().Add(time.Second * time.Duration(respToken.ExpiresIn-30)) // 提前30秒失效token
+
+		// 保存新的token
+		api.apiTokenStore.Set(api.apiToken, api.apiTokenExpireIn, api.apiTokenExpireAt)
+	}
+
+	return resp, errs
+}
+
+// IsValid 返回当前的token是否过期
+// token值存在并且没有到达有效期
+func (api *WechatAPI) IsValid() bool {
+	return isValid(api.apiToken, api.apiTokenExpireAt)
+}
+
+func isValid(apiToken string, apiTokenExpireAt time.Time) bool {
+	return (apiToken != "" && apiTokenExpireAt.After(time.Now()))
 }
--- applet.go
+++ applet.go
@@ -14,7 +14,6 @@ type (
 		appToken       string                   // 小程序token
 		encodingAESKey string                   // 小程序加密秘钥
 		API            *api.WechatAPI           // 小程序接口
-		Token          *api.WechatToken         // 小程序token服务
 		Crypto         *crypto.WechatCrypto     // 微信加密解密工具
 		Messager       *message.WechatMessenger // 小程序消息信使
 	}
@@ -22,8 +21,7 @@ type (
 
 // NewApplet 新建一个小程序实例
 func NewApplet(appID, appKey, appToken, encodingAESKey string, tokenStore api.WechatTokenStore) *Applet {
-	token := api.NewWechatToken(tokenStore)
-	apix := api.NewWechatAPI(appID, appKey, token)
+	apix := api.NewWechatAPI(appID, appKey, tokenStore)
 	crypto := crypto.NewWechatCrypto(appID, appToken, encodingAESKey)
 	messager := message.NewWechatMessager(crypto)
 
@@ -33,7 +31,6 @@ func NewApplet(appID, appKey, appToken, encodingAESKey string, tokenStore api.We
 		appToken:       appToken,
 		encodingAESKey: encodingAESKey,
 		API:            apix,
-		Token:          token,
 		Crypto:         crypto,
 		Messager:       messager,
 	}

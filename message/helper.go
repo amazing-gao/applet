ecc37e68aef8fb6f029aaf7eb3809e238fe9a222
bitebit <kfbygj@gmail.com>
2019-08-26 09:42:43 +0800

    feat(Message): 支持text/json,text/xml格式

--- message/helper.go
+++ message/helper.go
@@ -7,9 +7,9 @@ import (
 )
 
 func unmarshal(contentType string, rawMsg []byte, msg *Message) error {
-	if contentType == "application/json" {
+	if contentType == "application/json" || contentType == "text/json" {
 		return json.Unmarshal(rawMsg, msg)
-	} else if contentType == "application/xml" {
+	} else if contentType == "application/xml" || contentType == "text/xml" {
 		return xml.Unmarshal(rawMsg, msg)
 	}
 
